6599206b4cb52fa92a8953675243ca9465f14241
liangjieddd <37705899+liangjieddd@users.noreply.github.com>
2019-11-05 12:28:08 +0800

    Add files via upload

---
+++ demo.sh
@@ -0,0 +1,4 @@
+python demo/demo.py --config-file configs/PascalVOC-Detection/faster_rcnn_R_50_FPN.yaml
+  --input images/000144.jpg
+  --output results
+  --opts MODEL.WEIGHTS models/model_final_2d9806.pkl
